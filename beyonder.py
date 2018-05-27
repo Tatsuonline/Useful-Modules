373795550459fc872e85010c8dd0bf429c77450e
tatsu <tatsu@andromeda.attlocal.net>
2018-05-26 18:01:44 -0700

    Now with multiple parameters!

--- beyonder.py
+++ beyonder.py
@@ -10,10 +10,19 @@ def beyonder (test_function, test_dictionary):
         print("\n=== \033[94mTest Case# " + str(counter) + "\033[0m ===")
         print("Input: " + str(key))
         print("Expected Output: " + str(test_dictionary[key]))
-        print("Actual Output: " + str(test_function(key)))
-        if test_function(key) == test_dictionary[key]:
-            print("Result: \033[92mPASS\033[0m")
+        if type(key) is tuple: # Multiple Parameters
+            print("Actual Output: " + str(test_function(*key)))
+            if test_function(*key) == test_dictionary[key]:
+                print("Result: \033[92mPASS\033[0m")
+            else:
+                print("Result: \033[91;5mFAIL\033[0m")
+
         else:
-            print("Result: \033[91;5mFAIL\033[0m")
+            print("Actual Output: " + str(test_function(key)))
+            if test_function(key) == test_dictionary[key]:
+                print("Result: \033[92mPASS\033[0m")
+            else:
+                print("Result: \033[91;5mFAIL\033[0m")
+
 
         counter += 1
